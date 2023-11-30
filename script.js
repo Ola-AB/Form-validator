74ecea70d3b2232981fdc84a7287cd7744fbd795
Ola <olo2040@wp.pl>
2023-11-30 22:57:10 +0100

    added js file

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
-const username = document.querySelector('#username');
-const pass = document.querySelector('#password');
-const pass2 = document.querySelector('#password2');
-const email = document.querySelector('#email');
-const sendBtn = document.querySelector('.send');
-const clearBtn = document.querySelector('.clear');
-const popup = document.querySelector('.popup');
+const username = document.querySelector("#username");
+const pass = document.querySelector("#password");
+const pass2 = document.querySelector("#password2");
+const email = document.querySelector("#email");
+const sendBtn = document.querySelector(".send");
+const clearBtn = document.querySelector(".clear");
+const popup = document.querySelector(".popup");
 
+const showError = (input, msg) => {
+	const formBox = input.parentElement;
+	const errorMsg = formBox.querySelector(".error-text");
 
-const ShowError = (input, msg) => {
+	formBox.classList.add("error");
+	errorMsg.textContent = msg;
+};
 
+const clearError = (input) => {
+	const formBox = input.parentElement;
+	formBox.classList.remove("error");
+};
 
-    const formBox = input.parentElement;
-    const errorMsg = formBox.querySelector('.error-text');
+const checkForm = (input) => {
+	input.forEach((el) => {
+		if (el.value === "") {
+			showError(el, el.placeholder);
+		} else {
+			clearError(el);
+		}
+	});
+};
 
-    formBox.classList.add('error');
-    errorMsg.textContent = msg;
-}
+checkLength = (input, min) => {
+	if (input.value.length < min) {
+		showError(
+			input,
+			`${input.previousElementSibling.innerText.slice(
+				0,
+				-1
+			)} składa się z minimum ${min} znaków`
+		);
+	}
+};
 
-const clearError = input => {
-    const formBox = input.parentElement;
-    formBox.classList.remove('error');
+checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
+		showError(pass2, "Hasła do siebie nie pasują");
+	}
+};
 
-}
+checkMail = (email) => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+	if (re.test(email.value)) {
+		clearError(email);
+	} else {
+		showError(email, "Email jest niepoprawny");
+	}
+};
 
-const checkForm = input => {
+sendBtn.addEventListener("click", (e) => {
+	e.preventDefault();
 
-    input.forEach(el => {
-        if(el.value === '') {
-            ShowError(el, el.placeholder)
-        } else {
-            clearError(el);
-        }
-    })
-   
-}
+	checkForm([username, pass, pass2, email]);
+	checkLength(username, 5);
+	checkLength(pass, 8);
+	checkPassword(pass, pass2);
+	checkMail(email);
+});
 
-sendBtn.addEventListener('click', e => {
-    e.preventDefault();
+clearBtn.addEventListener("click", (e) => {
+	e.preventDefault();
 
-checkForm([username, pass, pass2, email])
-
-})
-
-
-clearBtn.addEventListener('click', e => {
-    e.preventDefault();
-
-    [username, pass, pass2, email].forEach(el => {
-        el.value = ''
-    });
-})
+	[username, pass, pass2, email].forEach((el) => {
+		el.value = "";
+	});
+});
